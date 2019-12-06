fb3865d7df68c8976f4531a17691349d62d71073
ManuelBeschi <manuel.beschi@gmail.com>
2019-12-06 14:16:14 +0100

    improve birrt, start working on path class

--- sample_based/birrt_extend.m
+++ sample_based/birrt_extend.m
@@ -7,21 +7,53 @@ classdef birrt_extend < handle
         goal_conf
         max_distance
         checker
+        start_tree
+        goal_tree
+        sampler
     end
     
     methods
-        function obj = birrt_extend(start_conf,goal_conf,max_distance,checker)
+        function obj = birrt_extend(start_conf,goal_conf,max_distance,checker,sampler)
             obj.start_conf = start_conf;
             obj.goal_conf=goal_conf;
             obj.max_distance=max_distance;
             obj.checker=checker;
+            
+            
+            start_node=node(start_conf);
+            goal_node=node(goal_conf);
+            obj.start_tree=tree(start_node,1,max_distance,checker);
+            obj.goal_tree=tree(goal_node,0,max_distance,checker);
+            
+            obj.sampler=sampler;
         end
         
-        function outputArg = solve(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function [solved,path] = step(obj)
+            path=[];
+            solved=false;
+            q=obj.sampler.sample;
+            [add_to_start,new_t1_node]=obj.start_tree.extend(q);
+            if (add_to_start)
+                [add_to_goal,new_t2_node]=obj.goal_tree.extendToNode(new_t1_node);
+            else
+                [add_to_goal,new_t2_node]=obj.goal_tree.extend(q);
+            end
+            if (add_to_goal && add_to_start && isequal(new_t1_node,new_t2_node))
+                solved=true;
+                path=[obj.start_tree.getConnectionToNode(new_t1_node) obj.goal_tree.getConnectionToNode(new_t1_node)];
+            end
         end
+        
+        function [solved,path] = solve(obj)
+            solved=false;
+            for idx=1:1000
+                [solved,path]=obj.step;
+                if (solved)
+                    return;
+                end
+            end
+        end
+        
     end
 end
 
