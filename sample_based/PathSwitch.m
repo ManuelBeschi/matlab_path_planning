8c5623e74a3fdc1e04c14b99d1ad78b6fff561ae
CesareTonola <58487086+CesareTonola@users.noreply.github.com>
2020-01-27 15:49:07 +0100

    Add files via upload

--- sample_based/PathSwitch.m
+++ sample_based/PathSwitch.m
@@ -11,7 +11,7 @@ function [new_path,success] = PathSwitch(current_path,other_paths,q,lb,ub,max_di
 %> max_distance: the max distance between node admissible for the BirrtExtend algorithm
 %> checker: the collision checker
 %> metrics: the metric used to calculate the connections cost
-%> opt_type ?????????????????????????????????
+%> opt_type: the type of path local optimization
 %> succ_node: if 1, the PathSwitch function looks for the best node of
 % all the other paths to connect to, oherwise it considers, for any other
 % path, the closest node.
@@ -23,11 +23,14 @@ function [new_path,success] = PathSwitch(current_path,other_paths,q,lb,ub,max_di
 % fino ad ora
 new_path = [];
 success = 0;
-
-path1_node = current_path.findCloserNode(q); %per ora fai così, è temporaneo %_____
-path_cost = inf;
 switch2path = 0;
 switch2node = 0;
+
+path1_node = current_path.findCloserNode(q); %per ora fai così, è temporaneo %_____
+path1_node2goal = current_path.getSubpathFromNode(path1_node);
+subpath1_cost = path1_node2goal.cost
+
+path_cost = inf;
 
 for j = 1:length(other_paths)
     
@@ -42,64 +45,96 @@ for j = 1:length(other_paths)
     else     
         path2_node_vector = path2.findCloserNode(path1_node.q);
     end
+    
     for k=1:length(path2_node_vector)
  
             path2_node = path2_node_vector(k);
 
             path2_node2goal = path2.getSubpathFromNode(path2_node);
             subpath2 = path2_node2goal.connections;
+            subpath2_cost = path2_node2goal.cost;
             
-            path1_node_fake = Node(path1_node.q);
-            path2_node_fake = Node(path2_node.q);
+            diff_subpath_cost = subpath1_cost-subpath2_cost;  %margine di costo concesso a connecting_path
+            distance_path_node = norm(path1_node.q-path2_node.q,2);
             
-            replan_sampler = InformedSampler(path1_node_fake.q,path2_node_fake.q,lb,ub);
-%             if path_cost<inf
-%             sampler.setCost(path_cost)
-%             end
-            solver_replannig = BirrtExtend(path1_node_fake,path2_node_fake,max_distance,checker,replan_sampler,metrics);
-            [~,replanned_path] = solver_replannig.solve; %check if solved
-            replanned_path.verboseDebug(false);
-            path_optimizer=PathLocalOptimizer(replanned_path,opt_type,checker,metrics);
-            path_optimizer.solve; 
-            connecting_path = path_optimizer.path.connections;
+            if (diff_subpath_cost>0 && distance_path_node<diff_subpath_cost) %se il costo della porzione rimanente di path1 è maggiore di quella di path2 ha senso verificare se muoversi verso path2 è vantaggioso in termini di costo
+                                                                             %se la distanza fra i due nodi è maggiore in termini di costo di quello concesso al connecting path devo escludere, meglio di una retta non posso fare..VALE SOLO SE LA METRICA E' LA DISTANZA               
+                path1_node_fake = Node(path1_node.q);
+                path2_node_fake = Node(path2_node.q);
             
-            node1 = connecting_path(1).getChild;
-            node2 = connecting_path(end).getParent;
-            conn1_cost = metrics.cost(path1_node,node1);
-            conn2_cost = metrics.cost(node2,path2_node);
-            conn1 = Connection(path1_node,node1,conn1_cost);
-            conn2 = Connection(node2,path2_node,conn2_cost);
-            
-            connecting_path = [conn1,connecting_path(2:end-1),conn2];
+                replan_sampler = InformedSampler(path1_node_fake.q,path2_node_fake.q,lb,ub,diff_subpath_cost); %uso l'ellisse con dimensione dettata dal costo massimo che il connecting path potrà avere, oltre a questo valore subpath1 conviene di più
+                %replan_sampler.setCost(diff_subpath_cost)
 
-            path1_node_fake.delete;
-            path2_node_fake.delete;
-    
-            conn_cost = 0;
-            for i=1:length(subpath2)
-                conn_cost=conn_cost+metrics.cost(subpath2(i).getParent,subpath2(i).getChild);
-            end
-            for i=1:length(connecting_path)
-                    conn_cost=conn_cost+metrics.cost(connecting_path(i).getParent,connecting_path(i).getChild);
-            end
-    
-            if(conn_cost<=path_cost && ~isempty(subpath2))
-                    new_path=Path([connecting_path subpath2]);
-                    switch2path = j;
-                    switch2node = k;
-                    path_cost = conn_cost;
-                    success = 1;
-            end 
+                solver_replannig = BirrtExtend(path1_node_fake,path2_node_fake,max_distance,checker,replan_sampler,metrics);
+                [solved,replanned_path] = solver_replannig.solve;
+                
+                if(solved) %se sono riuscito a pianificare un connecting path..
+                    replanned_path.verboseDebug(false);
+                    path_optimizer=PathLocalOptimizer(replanned_path,opt_type,checker,metrics);
+                    path_optimizer.solve; 
+                    connecting_path = path_optimizer.path.connections;
+            
+                    node1 = connecting_path(1).getChild;
+                    node2 = connecting_path(end).getParent;
+                    conn1_cost = metrics.cost(path1_node,node1);
+                    conn2_cost = metrics.cost(node2,path2_node);
+                    conn1 = Connection(path1_node,node1,conn1_cost);
+                    conn2 = Connection(node2,path2_node,conn2_cost);
+            
+                    connecting_path = [conn1,connecting_path(2:end-1),conn2];
 
+                    path1_node_fake.delete;
+                    path2_node_fake.delete;
+    
+                    conn_cost = subpath2_cost;
+                    for i=1:length(connecting_path)
+                        conn_cost=conn_cost+metrics.cost(connecting_path(i).getParent,connecting_path(i).getChild);
+                    end
+                    
+                    disp('path number, node number:')
+                    disp(j);
+                    disp(k);
+                    disp('cost:')
+                    disp(conn_cost)
+                    
+                    if(conn_cost<path_cost && conn_cost<subpath1_cost && ~isempty(subpath2)) %%DUBBIO %verifico sia che il costo del connecting path sia compatibile con il limite dato da diff_subpath_cost (altrimenti conviene subpath1) e verifico che il costo di questo connecting path sia minore di quelli precedenti
+                            new_path=Path([connecting_path subpath2]);
+                            switch2path = j;
+                            switch2node = k;
+                            path_cost = conn_cost;
+                            success = 1;
+                            disp('Connection  POSSIBLE')
+                            disp('-------------------------------------------');
+                    end 
+                else
+                    disp('Connection NOT POSSIBLE to the node number:')
+                    disp(k);
+                    disp('of the path number:')
+                    disp(j);
+                    disp('-------------------------------------------');
+                end
+            else
+                disp('It is better subpath1 than subpath2')
+                disp('path number:')
+                disp(j);
+                disp('node number:')
+                disp(k);
+                disp(diff_subpath_cost)
+                disp(distance_path_node)
+                disp('-------------------------------------------');
+            end
     end
 
 end
 disp('Connection to the path number:')
 disp(switch2path)
+disp('-------------------------------------------');
 if(succ_node == 1)
     disp('To the node number:')
     disp(switch2node)
+    disp('-------------------------------------------');
 end
 end
-
+%NB: SEGNALA CHE TALVOLTA QUALCH NODO DELLA RETE NON È CORRETTAMENTE
+%CONNESSO ALLA RETE (es non ha parent_connections)
 
